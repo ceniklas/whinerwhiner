b6be6c9ba1b2e62b20ae709d9101d82b7badb78b
Carolin Sellman <carolinsellman@Carolins-MacBook-Pro.local>
2019-11-18 18:58:33 +0100

    Initial design

--- App.tsx
+++ App.tsx
@@ -1,34 +1,64 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Animated, Easing, } from 'react-native';
 
 export default class App extends Component<{}, {}> {
+  spinValue;
+  spinAnimation;
+  componentWillMount = () => {
+    this.spinValue = new Animated.Value(0);
+    this.spinAnimation = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
+  }
+  spinMe = () => {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 3,
+        duration: 2000,
+        easing: Easing.inOut(Easing.exp),
+      }
+
+    )
+      .start()
+
+  }
+  onPressButton = () => {
+    this.spinMe()
+
+  }
   render() {
     return (
-      <View style={styles.container}>
+      <View style={WhinerWhinerStylesheet.container}>
 
-        <Text>Nisse? Vem är Nisse?</Text>
+        <Text>Nisse? Vem är Nisse??</Text>
 
-        <View style={styles.redBox}></View>
+        <Animated.View style={[
+          WhinerWhinerStylesheet.redBox,
+          { transform: [{ rotate: this.spinAnimation }] }
+        ]}>
 
-        <View style={styles.buttonWrapper}>
-          <Text>
-            Spin!
-          </Text>
-        </View>
+        </Animated.View>
+
+        <TouchableOpacity style={WhinerWhinerStylesheet.primaryButton} onPress={this.onPressButton}>
+          <Text>Spin!</Text>
+        </TouchableOpacity>
 
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const WhinerWhinerStylesheet = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'space-evenly',
   },
-  buttonWrapper: {
+  primaryButton: {
     paddingVertical: 16,
     paddingHorizontal: 24,
     backgroundColor: 'cyan',
