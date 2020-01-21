8eac69004a401b8a8afa55f915a4f9b26b57cf7c
Niklas Fransson <niklas.fransson@hiq.se>
2020-01-21 10:25:40 +0100

    GQL fe

--- App.tsx
+++ App.tsx
@@ -1,56 +1,70 @@
-import { StyleSheet, Text, View, TouchableOpacity, Animated, Easing, ImageBackground, AppRegistry, } from 'react-native';
-import React, { Component, useState } from 'react';
-import * as Font from 'expo-font';
-import { LinearGradient } from 'expo-linear-gradient';
-import { gql } from '@apollo/client';
-import { ApolloProvider, useSubscription } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-client';
-import { WebSocketLink } from 'apollo-link-ws';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { StyleSheet, Text, View, TouchableOpacity, Animated, Easing, ImageBackground, AppRegistry, } from 'react-native'
+import React, { Component, useState } from 'react'
+import * as Font from 'expo-font'
+import { LinearGradient } from 'expo-linear-gradient'
 
-import PrimaryButton from './src/components/common/PrimaryButton';
-import SpinWheel from './src/components/SpinWheel';
-import DefaultText from './src/components/common/DefaultText';
+import { gql, getMainDefinition } from '@apollo/client'
+import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-client'
+import { WebSocketLink } from 'apollo-link-ws'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
 
+import PrimaryButton from './src/components/common/PrimaryButton'
+import SpinWheel from './src/components/SpinWheel'
+import DefaultText from './src/components/common/DefaultText'
 
-const cache = new InMemoryCache();
-const link = new WebSocketLink({
-  uri: 'ws://localhost:4466/',
-});
-const client = new ApolloClient({link, cache});
+const websocketURL = 'ws://10.10.12.135:4466/'
+const httpURL = 'http://10.10.12.135:4466/'
+const cache = new InMemoryCache()
+
+const subsctiptionClient = new SubscriptionClient(websocketURL)
+
+const wsLink = new WebSocketLink(subsctiptionClient)
+const httpLink = new HttpLink({ uri: httpURL })
+
+const link = ApolloLink.split(
+  ({ query }) => {
+    const { kind, operation }: any = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink,
+)
+
+const client = new ApolloClient({ link, cache })
 
 const gqlQuery = gql`  
-subscription {
-  tweet{
-    mutation
-    node{
-      text
+  subscription {
+    tweet{
+      mutation
+      node{
+        text
+      }
     }
   }
-}
 `
 
-
 export default class App extends Component<{}, {}> {
-  
   state = {
+    online: false,
     fontLoaded: false,
     tweets: [],
-  };
-  
+    latestUpdate: {} as any
+  }
+
   TestComp = () => {
-    const { data, error, loading } = useSubscription(gqlQuery)
-    console.log(data, error, loading);
-    return (
-    <DefaultText>{data ? data.tweet.node.text : null}</DefaultText>
-    )
+    return (<>
+      <DefaultText>{this.state.online ? 'ONLINE' : 'OFFLINE'}</DefaultText>
+      <DefaultText>{this.state.latestUpdate.node ? this.state.latestUpdate.node.text : null}</DefaultText>
+    </>)
   }
 
   componentWillMount = () => {
-    // client.subscribe({})
-
-    
-    // client.subscribe({query: gqlQuery}).map(x => console.log('fjdkfgd', x))
+    subsctiptionClient.onDisconnected(() => this.setState({ online: false }))
+    subsctiptionClient.onConnected(() => this.setState({ online: true }))
 
     client.query({
       query: gql`
@@ -63,28 +77,27 @@ export default class App extends Component<{}, {}> {
           }
         }
       `
-    })
-    .then(result => this.setState({tweets: result.data.tweets}))
+    }).then(result => this.setState({ tweets: result.data.tweets }))
+
+    client.subscribe({ query: gqlQuery })
+    .subscribe(data => this.setState({latestUpdate: data.data.tweet}))
   }
 
   componentDidMount = async () => {
-    await this.loadFonts();
-    this.setState({ fontLoaded: true });
+    await this.loadFonts()
+    this.setState({ fontLoaded: true })
   }
 
   async loadFonts() {
     await Font.loadAsync({
       'source-sans-pro-bold': require('./assets/fonts/SourceSansPro-Bold.ttf'),
       'source-sans-pro-regular': require('./assets/fonts/SourceSansPro-Regular.ttf'),
-    });
+    })
   }
 
-  spinWheel = () => {}
-  
+  spinWheel = () => { }
 
   render() {
-    
-
     return this.state.fontLoaded ? (
       <ApolloProvider client={client}>
         <LinearGradient colors={['#2A126C', '#140A40']} style={WhinerWhinerStylesheet.container}>
@@ -92,22 +105,22 @@ export default class App extends Component<{}, {}> {
           <this.TestComp></this.TestComp>
 
           {/* <ImageBackground source={require('./assets/background.png')} style={WhinerWhinerStylesheet.container}> */}
-            
-            {/* <SpinWheel spinWheel={this.spinWheel}/> */}
 
-            <PrimaryButton onPress={this.spinWheel}>Spin</PrimaryButton>
+          {/* <SpinWheel spinWheel={this.spinWheel}/> */}
 
-            {this.state.tweets.map((tweet, idx) => (
-              <DefaultText key={idx}>
-                {tweet.text} - {tweet.author.name}
-              </DefaultText>
-            ))}
-            
+          <PrimaryButton onPress={this.spinWheel}>Spin</PrimaryButton>
+
+          {this.state.tweets.map((tweet, idx) => (
+            <DefaultText key={idx}>
+              {tweet.text} - {tweet.author.name}
+            </DefaultText>
+          ))}
+
           {/* </ImageBackground> */}
-          
+
         </LinearGradient>
       </ApolloProvider>
-    ) : null;
+    ) : null
   }
 }
 
@@ -117,6 +130,6 @@ const WhinerWhinerStylesheet = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'space-evenly',
   },
-});
+})
 
-AppRegistry.registerComponent('MyApplication', () => App);
+AppRegistry.registerComponent('MyApplication', () => App)
